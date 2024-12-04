50b33dd07345beb413fb3bf5eac5ca4eff20d956
vivekt007 <vivektigadi59@gmail.com>
2024-12-04 11:32:51 +0530

    test2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/reactTest/", // Base path for GitHub Pages
+    // base: "/reactTest/",
+    // Base path for GitHub Pages
     server: {
         port: 4000,
         host: "0.0.0.0",
