01acdb7800a5c8c8986b64ca1471de85536cdb23
vivekt007 <vivektigadi59@gmail.com>
2024-12-04 11:19:47 +0530

    "Update base path in vite.config.js from '/' to '/reactTest/'"

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/", // Base path for GitHub Pages
+    base: "/reactTest/", // Base path for GitHub Pages
     server: {
         port: 4000,
         host: "0.0.0.0",
