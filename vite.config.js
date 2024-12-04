4a5a7a9d4c6d60e4dce5d8b41dcfbbc80575f62c
vivekt007 <vivektigadi59@gmail.com>
2024-12-04 11:16:46 +0530

    "Update package.json and vite.config.js files"

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    base: "/", // Base path for GitHub Pages
+    server: {
+        port: 4000,
+        host: "0.0.0.0",
+        logLevel: "silent",
+        client: {
+            logging: "none",
+        },
+        assetsInclude: ["**/*.ttf", "**/*.png"],
+    },
+});
